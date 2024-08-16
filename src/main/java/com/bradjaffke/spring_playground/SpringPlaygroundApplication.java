2cfaf7c1f5e70cda541c5e8da43f1673d7c44f70
Brad Jaffke <bjaffke@gmail.com>
2024-08-16 11:49:06 -0700

    basic GET call using WebClient

--- src/main/java/com/bradjaffke/spring_playground/SpringPlaygroundApplication.java
+++ src/main/java/com/bradjaffke/spring_playground/SpringPlaygroundApplication.java
@@ -1,13 +1,30 @@
 package com.bradjaffke.spring_playground;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
 
 @SpringBootApplication
 public class SpringPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPlaygroundApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundApplication.class, args);
+
+		WebClient webClient = WebClient.create("https://api.chucknorris.io/jokes/random");
+
+		final String chuckNorrisBaseAPIURL = "https://api.chucknorris.io/jokes/random";
+
+		String chuckNorrisJoke = webClient.get()
+				.uri(chuckNorrisBaseAPIURL)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+		System.out.println(chuckNorrisJoke);
+
+	}
 }
